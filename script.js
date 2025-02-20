8b0fba04db35bdf486108f5205cd82a80bcc447c
hiteshsaini2332 <hiteshsaini9466@gmail.com>
2025-02-20 12:18:46 +0530

    Made loader Successfully

---
+++ script.js
@@ -0,0 +1,51 @@
+
+var h5timer=document.querySelector("#line1-part1 h5");
+var grow=0;
+var inter=setInterval(function(){
+    if(grow<100)
+    {
+        h5timer.innerHTML=grow++
+    }
+    else{
+        h5timer.innerHTML=grow
+    }
+    
+},33)
+
+var tl=gsap.timeline();
+tl.from(".line h1", {
+  y: 150,
+  duration: 0.6,
+  delay: 0.5,
+  stagger: 0.25,
+});
+tl.from("#line1-part1", {
+    opacity:0,
+    onStart:function(){
+        setInterval(function () {
+          if (grow < 100) {
+            h5timer.innerHTML = grow++;
+          } else {
+            h5timer.innerHTML = grow;
+          }
+        }, 33);
+    }
+});
+tl.to(".line h2",{
+    animationName:"anime",
+    opacity:1
+})
+tl.to(".loader",{
+    opacity:0,
+    duration:0.2,
+    delay:4
+})
+tl.from(".page1",{
+    delay:0.2,
+    y:1600,
+    duration:0,
+    ease:Power4
+})
+tl.to(".loader",{
+    display:"none"
+})
